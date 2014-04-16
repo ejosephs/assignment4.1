b2d5bd3d7d431e5d2c389580e7371037a71cc4e4
cs008-2014 <ejosephs@uvm.edu>
2014-04-16 17:04:02 -0400

    Create locations.php

---
+++ locations.php
@@ -0,0 +1,25 @@
+<?php 
+ include ("top.php");
+ ?>
+ 
+<article id="main">
+
+<h1>Locations</h1>
+
+<p>Ah.</p>
+
+</article>
+
+<!-- ###########   Article about the rules          z         ################## -->
+<aside id="rightColumn">
+<h1>Aside</h1>
+
+<p>Aside Paragraph</p>
+</aside>
+
+<!-- ###########    Footer                               ################## -->
+
+<?php include("footer.php")?>
+
+</body>
+</html>
