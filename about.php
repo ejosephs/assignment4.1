409674c1f91da3dc21056b94cc0154d471ad23a6
cs008-2014 <ejosephs@uvm.edu>
2014-04-16 17:03:36 -0400

    Create about.php

---
+++ about.php
@@ -0,0 +1,25 @@
+<?php 
+ include ("top.php");
+ ?>
+ 
+<article id="main">
+
+<h1>About Our Resort</h1>
+
+<p>Ah. Game of Thrones. So Throney.</p>
+
+</article>
+
+<!-- ###########   Article about the rules          z         ################## -->
+<aside id="rightColumn">
+<h1>Aside</h1>
+
+<p>Aside Paragraph</p>
+</aside>
+
+<!-- ###########    Footer                               ################## -->
+
+<?php include("footer.php")?>
+
+</body>
+</html>
