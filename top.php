e8385cdcad963a634fb1ad1b842eb935aab2c6ff
cs008-2014 <ejosephs@uvm.edu>
2014-04-16 17:02:02 -0400

    Create top.php

---
+++ top.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Game of Thrones Restaurant</title>
+<meta charset="utf-8">
+<meta name="author" content="Erica Josephson">
+<meta name="description" content="Feast Yourself">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!--[if lt IE 9]>
+	<script src="//html5shim.googlecode.com/sin/trunk/html5.js"></script>
+<![endif]-->
+	
+<link rel="stylesheet" 
+      href="mystyle.css" 
+      type="text/css" 
+      media="screen">
+
+</head>
+
+<?php
+print '<body id="' . $path_parts['filename'] . '">';
+
+include ("header.php");
+include ("nav.php");
+?>
