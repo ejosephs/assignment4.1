6e68c41a39b90e616832a52f955b86674203e633
cs008-2014 <ejosephs@uvm.edu>
2014-04-16 19:52:00 -0400

    Update home.php

--- home.php
+++ home.php
@@ -4,17 +4,22 @@
  
 <article id="main">
 
-<h1>Our Resort</h1>
+<h1>A Feast For Crows</h1>
 
-<p>Ah.</p>
+<p>Dinner is coming and it is time to dine like a King. A Feast For Crows is the finest eatery in Westeros.
+Featuring Farkas Tulain, head chef to the late King Joffery ( May His Grace rest in peace), our restuarant has
+an extensive menu.  With food approved by the Gods, Old and New, A Feast For Crows is the obvious choice for 
+anyone looking to dine in King's Landing. Whether you have traveled near or far, all kinds are welcome in our
+establishment.</p>
 
 </article>
 
 <!-- ###########   Article about the rules          z         ################## -->
 <aside id="rightColumn">
-<h1>Aside</h1>
+<h1>We Are Not Alone</h1>
 
-<p>Aside Paragraph</p>
+<p>King's Landing is not our only location.  We have a tavern and inn located in Winterfell, mead hall and grill
+in the Iron Islands(Home to Theon's Famous Smoked Sausage), and a bistro in the Vale. </p>
 </aside>
 
 <!-- ###########    Footer                               ################## -->
