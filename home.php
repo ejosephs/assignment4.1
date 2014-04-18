77aff32d4dbed570d239d23e98465cc2fd546a85
cs008-2014 <ejosephs@uvm.edu>
2014-04-17 21:51:17 -0400

    Update home.php

--- home.php
+++ home.php
@@ -4,24 +4,24 @@
  
 <article id="main">
 
-<h1>Seven Kingdoms</h1>
-<h2> A Feast For Crows</h2>
+<h1> A Feast For Crows</h1>
 
 <p>Dinner is coming and it is time to dine like a King. Seven Kingdoms is the finest eatery in Westeros.
 Featuring Farkas Tulain, head chef to the late King Joffery ( May His Grace rest in peace), our restuarant has
 an extensive menu.  With food approved by the Gods, Old and New, Seven Kingdoms is the obvious choice for 
 anyone looking to dine in King's Landing. Whether you have traveled near or far, all kinds are welcome in our
 establishment.</p>
-
+<img src="images/Food.gif" height="256" width="488" alt="Dining at the Seven Kingdoms"/>
 </article>
 
-<!-- ###########   Article about the rules          z         ################## -->
+<!-- ###########   Article          ################## -->
 <aside id="rightColumn">
 <h1>We Are Not Alone</h1>
 
 <p>King's Landing is not our only location.  We have a tavern and inn located in Winterfell, mead hall and grill
 in the Iron Islands(Home to Theon's Famous Smoked Sausage), and a bistro in the Vale. </p>
 </aside>
+<img src="images/Tavern.gif" height="325" width="488" alt="Dining at the Seven Kingdoms"/>
 
 <!-- ###########    Footer                               ################## -->
 
