123b6d0cbebecd852ac85da26bfba52428712af7
cs008-2014 <ejosephs@uvm.edu>
2014-04-21 12:28:06 -0400

    Update locations.php

--- locations.php
+++ locations.php
@@ -10,19 +10,32 @@
 <p>Our primary location is Seven Kingdoms in Kings Landing.  It is the most upscale of our establishments. You
 will find no better food or experience in all of Westeros.  Whether you are looking to impress a date, have a fine
 wine, or be served in the lap of elegance, please visit us.</p>
+<p> </p>
+<p> </p>
+<p> </p>
+<p> </p>
 
 <p>A Clash of Kings</p>
 <p>Winter is coming but that does not mean you should be cold.  A Clash For Kings is a homely tavern located in
 Winterfell.  Whether you want a full, hearty meal or just a drink to escape from the cold, A Clash For Kings is
 an excellent choice for anyone who finds themself in the north.  Our warm, homely atmosphere and fantastic ale are
 guaranteed to even warm the heart of a white walker.</p>
+<p> </p>
+<p> </p>
+<p> </p>
+<p> </p>
 
 <p>Greyjoy's Iron and Ale</p>
 <p>Meat does not require planting or harvesting.  So it makes sense that Iron and Ale is in the land of the people
 that "Do Not Sow".  Meat and mead is their specialty. Do  not go expecting other options.  Although constrictive due
 to their inability to grow crops, their menu features many fantastic options including Theon's Smoked Sausage (A 
 recipe created by Ramsey Snow).  Anyone looking to taste the greatest meat in all of Westeros will not be disappointed
-at Iron and Ale.</p>
+at Iron and Ale.
+</p>
+<p> </p>
+<p> </p>
+<p> </p>
+<p> </p>
 
 <p>Catelyn's Bistro</p>
 <p>Established in the memory of the late Catelyn Stark, Catelyn's Bistro is a small, quaint restuarant located in The Fingers,
@@ -31,12 +44,14 @@ very upscale while still welcoming every walk of life (as long as they can still
 Catelyn's Bistro makes well for a romantic rendevous or a quiet dinner.</p>
 </article>
 
-<!-- ###########   Article about the rules          z         ################## -->
-<aside id="rightColumn">
-<h1>Aside</h1>
 
-<p>Aside Paragraph</p>
+<aside>
+    <p><img src="images/Tavern.gif" height="200" width="300" alt="Seven Kingdoms"></p>
+    <p><img src="images/Rest1.gif" height="200" width="300" alt="A Clash of Kings"></p>
+    <p><img src="images/Rest2.jpg" height="200" width="300" alt="Greyjoy's Iron and Ale"></p>
+    <p><img src="images/Rest3.gif" height="200" width="300" alt="Catelyn's Bistro"></p>
 </aside>
+
 
 <!-- ###########    Footer                               ################## -->
 
