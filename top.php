aa64a0c9bf8cdd0a08de176b0d876c40edc0088a
cs008-2014 <ejosephs@uvm.edu>
2014-04-21 12:25:19 -0400

    Update top.php

--- top.php
+++ top.php
@@ -16,7 +16,30 @@
       href="mystyle.css" 
       type="text/css" 
       media="screen">
+<?php
+if($_SERVER['HTTPS']) {
+    $domain = "https://www.uvm.edu";
+}else{
+    $domain = "http://www.uvm.edu";
+}
 
+$server = htmlentities($_SERVER['SERVER_NAME'], ENT_QUOTES, "UTF-8");
+$domain .= $server;
+$phpSelf = htmlentities($_SERVER['PHP_SELF'], ENT_QUOTES, "UTF-8");
+$path_parts = pathinfo($phpSelf);
+$basePath = $domain . $path_parts['dirname'] . "/";
+
+if ($debug){
+    print "<p>Domain". $domain;
+    print "<p>php Self". $phpSelf;
+    print "<p>basePath". $basePath;
+    print "<p>Path Parts<pre>";
+    print_r($path_parts);
+    print "</pre>";
+}
+
+require_once('lib/security.php');
+?>
 </head>
 
 <?php
