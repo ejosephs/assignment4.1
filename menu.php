45388a09f0538952ded512ca43d809093585af1f
cs008-2014 <ejosephs@uvm.edu>
2014-04-21 12:27:32 -0400

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,20 +1,381 @@
-<?php 
+?php 
  include ("top.php");
  ?>
  
 <article id="main">
 
-<h1>Events</h1>
+<h1>Breakfast</h1>
+<ul>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Breakfast at Winterfell</label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>
+        <label>Melon and hard-cooked eggs</label>
+        <span>4576</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>
+        <label>Honeycakes w. Blackberries</label>
+        <span>4320</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>
+        <label>Teff Porridge w. honey and dates</label>
+        <span>5.79</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>
+        <label>Baked Currant Doughnuts</label>
+        <span>4.59</span>
+        
+</ul>
 
-<p>Ah.</p>
+<h1>Soups & Salads</h1>
+<ul>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Greens Dressed w.Apples & Pine Nuts</label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Autumn Greens </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Salad of green beans, onions, and beets </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Beef & Barley Stew </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Creamy Chestnut Soup </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Ginger Soup </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Onion Broth </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Root Soup </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Venison Stew </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Rosehip Soup </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Leek soup </label>
+        <span>9999</span>
+    </li>
+</ul>
+<h1>Appetizers</h1>
+<ul>    
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>White beans and bacon </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Spiced Squash </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Cod Cakes </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Mushrooms in Garlic </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Boiled beans </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Fiddlehead Ferns </label>
+        <span>9999</span>
+    </li>
+</ul>
+
+<h1>Dinner</h1>
+<ul>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Honeyed Chicken </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Crown Roast </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Goose sauces w. Mulberries </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Hen on the Wall </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Trout baked in Clay </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Potted Hare </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Haunch of Goat </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Mutton Chops sauces w. Honey </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Cold Capon </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Ham Studded w. Cloves </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Simple Dornish Fare </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Rack of Venison </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Boiled Beef and Horseradish </label>
+        <span>9999</span>
+    </li>
+</ul>
 
 </article>
 
 <!-- ###########   Article about the rules          z         ################## -->
 <aside id="rightColumn">
-<h1>Aside</h1>
+<h1>Drinks</h1>
+<h2>Alcoholic</h2>
+<ul>    
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Wildling Cider </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Wassail </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Pear Brandy </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Persimmon Wine </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Sweetwine </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Honeyed wine </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Plum wine </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Mead </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Beer </label>
+        <span>9999</span>
+    </li>
+</ul>
 
-<p>Aside Paragraph</p>
+<h2>Non-Alcoholic</h2>
+<ul>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Sweet Sleep </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Mint Sun Tea </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Nettle Tea </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Milk Tea </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Almond Milk </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Ice milk sweetened w. honey </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Mock-Sahlep </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Lemonsweet </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Strawberry Lemonsweet </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Sekanjabin </label>
+        <span>9999</span>
+    </li>
+</ul>
+
+<h1>Desserts</h1>
+<ul>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Strawberries and Sweetgrass </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Milk Snow </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Peach Crostata </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Apple Crisps </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Strawberry Tart </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Lemon cakes </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Weirwood Cake </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Honeycakes </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Dornish Cream Cakes </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Chocolate Carrageen Pudding </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Sherbet </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Stewed Plums </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Sweet Biscuits </label>
+        <span>9999</span>
+    </li>
+    <li>
+        <div class="dotted-bg"></div>    
+        <label>Candied Ginger </label>
+        <span>9999</span>
+    </li>
+</ul>
+
 </aside>
 
 <!-- ###########    Footer                               ################## -->
