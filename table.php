32486a0fd8716b0e770fcd959c7a3bf74bc505f0
cs008-2014 <ejosephs@uvm.edu>
2014-04-22 16:47:17 -0400

    Update table.php

--- table.php
+++ table.php
@@ -1,7 +1,16 @@
 <?php 
 include ("top.php"); 
 ?>
+<?php
+    $h=date('G');
+    $d=date('D');    
 
+    if($d=='Mon'or $d=='Tue'or $d=='Wed'or $d=='Thu' and $h<20 and $h>9) $img= 'open.jpg';
+    else if ($d=='Fri' or $d=='Sat' and $h>9 and $h<24) $img='open.jpg';
+    else if($d=='Sun' and $h>10 and $h<6) $img='open.jpg';
+    else $img='closed.jpg';
+?>
+<img src="<?php echo $img;?>">
 <article id="main">
 	<h1>Hours:</h1>
 </article>
