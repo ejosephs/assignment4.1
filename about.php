b18fd47d317160b4293d25038c341d15d9e01ede
cs008-2014 <ejosephs@uvm.edu>
2014-04-21 20:03:28 -0400

    Update about.php

--- about.php
+++ about.php
@@ -4,17 +4,40 @@
  
 <article id="main">
 
-<h1>About Our Resort</h1>
+<h1>About Seven Kingdoms</h1>
 
-<p>Ah. Game of Thrones. So Throney.</p>
+<p>We are located in the heart of King's Landing, a wonderful destination to anyone visiting or staying in
+Westeros.  With the finest chefs in the land, our specialty is cooking.  Our family of staff are all ready to 
+welcome you into our restaurant and serve on you hand and foot.  We also host several events including self
+and outside sponsored gatherings.  We are able also able to make housing accomadations for extended stays
+(space is however limited).</p>
 
 </article>
 
 <!-- ###########   Article about the rules          z         ################## -->
 <aside id="rightColumn">
-<h1>Aside</h1>
+<h1>Events</h1>
 
-<p>Aside Paragraph</p>
+<p>We host regular events in the Seven Kingdoms! Also own a large field outside the walls of King's Landing
+named Lord's Field where we host larger gatherings.
+Here is a look at some upcoming events:
+
+When: the 11th
+Where: Seven Kingdoms
+What: New Season Cider Tasting (all drinks 15% OFF)
+
+When: the 14th
+Where: Seven Kingdoms
+What: Fletchers' Guild Banquet (Closed to the Public)
+
+When: the 21st
+Where: Lord's Field
+What: Day of Games. Including: Jousting Tournament
+
+When: the 26th
+Where: Seven Kingdoms
+What: Battle of the Chefs (Cook from Dorne vs Our Very Own)
+</p>
 </aside>
 
 <!-- ###########    Footer                               ################## -->
